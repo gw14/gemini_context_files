65dece9a05fb84fff5504e3ce633f0ec74061523
Guy Waldman <waldmanguy@gmail.com>
2026-02-01 12:04:27 +0200

    focusing on cka golang powershell until may 1st, all else is archived

--- tasks/powershell/Define_a_Basic_Function_Jan2626.ps1
+++
@@ -1,9 +0,0 @@
-function Get-AppStatus{
-	#A function that finds a specific process and returns basic info about it.
-	param(
-	[Parameter(Mandatory=$true)]
-	[string]$ProcessName
-	)
-	
-	get-process $ProcessName
-}
